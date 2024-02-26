b4dc02578f763a4255e55b1b747604198278d261
Narendra <narendrakotika3576@gmail.com>
2024-02-26 19:04:14 +0530

    Add project files.

---
+++ CourseEndPizza/Pages/OrderCheckout.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class OrderCheckoutModel : PageModel
+{
+    [BindProperty(SupportsGet = true)]
+    public string SelectedPizza { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public int Quantity { get; set; }
+
+    public double TotalPrice => CalculateTotalPrice();
+
+    public void OnGet(string pizza, int quantity)
+    {
+        SelectedPizza = pizza;
+        Quantity = quantity;
+    }
+
+    public IActionResult OnPostPlaceOrder()
+    {
+        
+        return RedirectToPage("OrderConfirmation", new { orderId = 123, amount = TotalPrice, pizza = SelectedPizza });
+    }
+
+    private double CalculateTotalPrice()
+    {
+       
+        double pizzaPrice = 10.0;
+        return Quantity * pizzaPrice;
+    }
+}
+
